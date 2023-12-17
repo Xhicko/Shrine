c0d77a4fb4c8d38b36d257352b16ff7e329a3c6a
Xhicko <otagadaisaac@gmail.com>
2023-12-17 22:37:32 +0100

    Created 3rd App File

---
+++ third-app/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {createRoot}  from 'react-dom/client'
+
+
+
+class App extends React.Component{
+
+    render(){
+        return pass
+    }
+}
+
+const root = createRoot(document.querySelector('#root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
